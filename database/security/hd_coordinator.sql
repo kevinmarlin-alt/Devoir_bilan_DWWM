3597bdc07f18d8f0e58670b510ac57b352b712d6
Kevin Marlin <kevin.marlin@gmail.com>
2026-09-14 20:19:42 +0200

    database(privileges): Add all necessary access rights for the coordinator

---
+++ database/security/hd_coordinator.sql
@@ -0,0 +1,51 @@
+-- =========================================================
+-- Harmonie Domicile
+-- Security rules for hd_coordinator
+-- =========================================================
+
+
+-- =========================================================
+-- SCHEMA ACCESS
+-- =========================================================
+
+GRANT USAGE ON SCHEMA public TO hd_coordinator;
+
+
+-- =========================================================
+-- GLOBAL READ ACCESS
+-- =========================================================
+
+
+GRANT SELECT
+ON
+    employee,
+    contract,
+    employee_skill,
+    employee_geographic_area,
+    skill,
+    geographic_area,
+    intervention_skill,
+    service_type,
+    unavailability,
+    beneficiary,
+    location
+TO hd_coordinator;
+
+
+-- =========================================================
+-- INTERVENTION
+-- =========================================================
+
+GRANT SELECT, INSERT, UPDATE ON intervention TO hd_coordinator;
+
+-- =========================================================
+-- ASSIGNMENT
+-- =========================================================
+
+GRANT SELECT, INSERT, UPDATE ON assignment TO hd_coordinator;
+
+-- =========================================================
+-- INTERVENTION SKILLS
+-- =========================================================
+
+GRANT SELECT, INSERT, DELETE ON intervention_skill TO hd_coordinator;
