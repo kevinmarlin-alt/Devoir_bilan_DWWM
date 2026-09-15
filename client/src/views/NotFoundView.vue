389e54f7660ee69b77677ad60e6804895c7419d4
Kevin Marlin <kevin.marlin@gmail.com>
2026-09-15 15:37:10 +0200

    build(router): add two pages to try if router works

---
+++ client/src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <section>
+        <h1>404</h1>
+
+        <p>La page demandée n'existe pas.</p>
+
+        <RouterLink to="/">
+            Retour à l'accueil
+        </RouterLink>
+    </section>
+</template>
