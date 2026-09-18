8ba27d568a187d98364f9fb63fc7e8c2f6bac805
Kevin Marlin <kevin.marlin@gmail.com>
2026-09-18 09:12:51 +0200

    feat(auth): add handleConnectedUser to retrieve authenticated user details

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -24,7 +24,7 @@ const loginSchema = {
 const authRouter = async (app: FastifyInstance) => {
     app.post("/login", { schema: loginSchema }, authController.handleLogin)
     app.post("/logout", authController.handleLogout)
-    
+    app.get('/user', authController.handleConnectedUser)
 };
 
 
