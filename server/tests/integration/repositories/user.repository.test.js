21a90febd2fadd757c694b2726d01437ffc8d2e6
Kevin Marlin <kevin.marlin@gmail.com>
2026-09-23 01:22:39 +0200

    refactor: add disconnect pool after tests

--- server/tests/integration/repositories/user.repository.test.js
+++ server/tests/integration/repositories/user.repository.test.js
@@ -1,9 +1,16 @@
-import { describe, expect, it } from "vitest";
+import { afterAll, describe, expect, it } from "vitest";
 import { findUserByEmail } from "../../../src/repositories/user.repository.js";
+import pool from "../../../src/database/database.js";
 
 describe('User repositories', () => {
 
-    it('retourne un utilisateur si l\'adresse e-mail est correct', async () => {
+
+    afterAll(async () => {
+        await pool.end()
+    })
+
+
+    it('retourne un utilisateur si l\'adresse e-mail est correcte', async () => {
         const response = await findUserByEmail('alice.martin@harmonie.test');
 
         expect(response).toHaveProperty("userAccountId")
@@ -14,9 +21,15 @@ describe('User repositories', () => {
 
         expect(response).toHaveProperty("passwordHash")
         expect(response.passwordHash).toBeTypeOf('string')
-        
+
         expect(response).toHaveProperty("isActive")
         expect(response.isActive).toBeTypeOf('boolean')
 
+    });
+
+    it('retourne null si l\'adresse email est incorrecte', async () => {
+        const response = await findUserByEmail('unknow@harmonie.test');
+
+        expect(response).toBeNull();
     })
 })
