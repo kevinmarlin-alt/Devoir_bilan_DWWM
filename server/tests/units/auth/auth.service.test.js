bc99e1bfc11cf269e71dbeefc3bd0a5d90439115
Kevin Marlin <kevin.marlin@gmail.com>
2026-09-23 10:00:28 +0200

    refactor: update the user authentification tests

--- server/tests/units/auth/auth.service.test.js
+++ server/tests/units/auth/auth.service.test.js
@@ -1,9 +1,18 @@
 import { describe, expect, it, vi } from 'vitest';
+import bcrypt from 'bcrypt';
 import { authenticateUser } from '../../../src/services/auth.service.js';
 import { findUserByEmail } from '../../../src/repositories/user.repository.js';
 
+
 vi.mock('../../../src/repositories/user.repository.js', () => ({
     findUserByEmail: vi.fn()
+}));
+
+
+vi.mock('bcrypt', () => ({
+    default: {
+        compare: vi.fn()
+    }
 }));
 
 describe('authenticateUser', () => {
@@ -21,4 +30,73 @@ describe('authenticateUser', () => {
 
     });
 
+    it("refuse l'authentification si l'utilisateur est inactif", async () => {
+
+        findUserByEmail.mockResolvedValue({
+            userAccountId: 1,
+            email: "inconnu@harmonie.test",
+            passwordHash: "5xtcFcjs....",
+            isActive: false
+
+        });
+
+        await expect(
+            authenticateUser(
+                'inconnu@harmonie.test',
+                'password'
+            )
+        ).rejects.toThrow('Identifiants incorrects');
+
+    });
+
+    it("refuse l'authentification si le mot de passe est incorrect", async () => {
+
+        findUserByEmail.mockResolvedValue({
+            userAccountId: 1,
+            email: "inconnu@harmonie.test",
+            passwordHash: "5xtcFcjs....",
+            isActive: true
+
+        });
+
+        bcrypt.compare.mockResolvedValue(false);
+
+        await expect(
+            authenticateUser(
+                'inconnu@harmonie.test',
+                'password'
+            )
+        ).rejects.toThrow('Identifiants incorrects');
+
+         expect(bcrypt.compare).toHaveBeenCalledWith(
+            'password',
+            '5xtcFcjs....'
+        );
+
+    });
+
+    it("confirme l'authentification et retourne les informations utiles de l'utilisateur", async () => {
+
+        findUserByEmail.mockResolvedValue({
+            userAccountId: 1,
+            email: "inconnu@harmonie.test",
+            passwordHash: "5xtcFcjs....",
+            isActive: true
+
+        });
+
+        bcrypt.compare.mockResolvedValue(true);
+
+        expect(
+            await authenticateUser(
+                'inconnu@harmonie.test',
+                'password'
+            )
+        ).toMatchObject({
+            userAccountId: 1,
+            email: 'inconnu@harmonie.test',
+            isActive: true
+        })
+
+    });
 });
