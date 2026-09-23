67c374026456a97d99ec6bb15d30d686dd14f801
Kevin Marlin <kevin.marlin@gmail.com>
2026-09-23 15:39:57 +0200

    refactor: add JWT authentication token creation and set cookie in login response

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { authenticateUser } from "../services/auth.service.js";
+import { createAuthToken } from "../services/token.service.js";
 
 export const handleLogin = async (req, res, next) => {
     
@@ -6,6 +7,16 @@ export const handleLogin = async (req, res, next) => {
         const { email, password } = req.body;
 
         const user = await authenticateUser(email, password);
+
+        const token = createAuthToken(user);
+
+        res.cookie('hd_token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            path: '/',
+            maxAge: 8 * 60 * 60 * 1000
+        })
 
         res.status(200).json({ user })
 
